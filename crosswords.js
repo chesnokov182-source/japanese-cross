49a76d779a6cf84aeeddc8df483223565e8ee7b3
chesnokov182-source <chesnokov182@gmail.com>
2026-04-01 08:42:57 +0500

    Update crosswords.js

--- crosswords.js
+++ crosswords.js
@@ -165,7 +165,7 @@ window.crosswordsData = {
                     { word: "チリトリ", row: 0, col: 3, dir: "down", clue: "Совок", number: 7},
                 ]
             },
-                        {
+            {
                 name: "Кроссворд 2",
                 width: 4,
                 height: 4,
@@ -179,6 +179,22 @@ window.crosswordsData = {
                     { word: "ウマ", row: 3, col: 2, dir: "across", clue: "Лошадь", number: 5},
                     { word: "クマ", row: 2, col: 3, dir: "down", clue: "Медведь", number: 6},
                     { word: "ホウソウ", row: 0, col: 2, dir: "down", clue: "Трансляция, вещание", number: 7},
+                ]
+            },
+            {
+                name: "Кроссворд 3",
+                width: 4,
+                height: 4,
+                price: 60,
+                words: [
+                    { word: "アシ", row: 0, col: 0, dir: "across", clue: "Нога", number: 1},
+                    { word: "アサヒ", row: 0, col: 0, dir: "down", clue: "Утреннее солнце", number: 1.1},
+                    { word: "シイタケ", row: 0, col: 1, dir: "down", clue: "Гриб шиитаке", number: 2},
+                    { word: "ヒタイ", row: 2, col: 0, dir: "across", clue: "Лоб", number: 3},
+                    { word: "サイコロ", row: 1, col: 0, dir: "across", clue: "Игральная кость", number: 4},
+                    { word: "コイシ", row: 1, col: 2, dir: "down", clue: "Маленький камень", number: 5},
+                    { word: "シロ", row: 0, col: 3, dir: "down", clue: "Белый цвет", number: 6},
+                    { word: "ケシキ", row: 3, col: 1, dir: "across", clue: "Пейзаж, вид", number: 7},
                 ]
             },
         ]
