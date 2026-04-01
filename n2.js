2b3743c8b22539c5fc3487efdc0f2bf532b2d015
chesnokov182-source <chesnokov182@gmail.com>
2026-04-01 08:48:27 +0500

    Update n2.js

--- n2.js
+++ n2.js
@@ -1,1 +1,21 @@
-
+// Кроссворды уровня N2
+window.crosswordsData = window.crosswordsData || {};
+window.crosswordsData.n2 = {
+    name: "JLPT N2",
+    puzzles: [
+        {
+            name: "Кроссворд 1",
+            width: 10,
+            height: 10,
+            price: 0,
+            words: [
+                { word: "チョウサ", row: 0, col: 3, dir: "across", clue: "Расследование, исследование" },
+                { word: "ハッテン", row: 2, col: 1, dir: "across", clue: "Развитие" },
+                { word: "カンリョウ", row: 4, col: 2, dir: "across", clue: "Чиновник, бюрократ" },
+                { word: "コクサイ", row: 6, col: 0, dir: "across", clue: "Международный" },
+                { word: "ジョウホウ", row: 1, col: 5, dir: "down", clue: "Информация" },
+                { word: "イノベーション", row: 3, col: 7, dir: "down", clue: "Инновация" }
+            ]
+        }
+    ]
+};
