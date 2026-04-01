d45e1fd60467fff28ee32625112712e8335edc73
chesnokov182-source <chesnokov182@gmail.com>
2026-04-01 08:47:58 +0500

    Update n4.js

--- n4.js
+++ n4.js
@@ -1,1 +1,55 @@
-
+window.crosswordsData = window.crosswordsData || {};
+window.crosswordsData.n4 = {
+  n4: {
+        name: "JLPT N4",
+        puzzles: [
+            {
+                name: "Кроссворд 1",
+                width: 4,
+                height: 4,
+                price: 0,
+                words: [
+                    { word: "カナヅチ", row: 0, col: 0, dir: "across", clue: "Молоток", number: 1},
+                    { word: "カイ", row: 0, col: 0, dir: "down", clue: "Раковина, моллюск", number: 1.1},
+                    { word: "イワ", row: 1, col: 0, dir: "across", clue: "Скала, риф", number: 2},
+                    { word: "ナワトビ", row: 0, col: 1, dir: "down", clue: "Прыжки через скакалку", number: 3},
+                    { word: "トマト", row: 2, col: 1, dir: "across", clue: "Томат, помидор", number: 4},
+                    { word: "ユビキリ", row: 3, col: 0, dir: "across", clue: "Делают мизинцами в знак обещания", number: 5},
+                    { word: "マキ", row: 2, col: 2, dir: "down", clue: "Огуречный суши-ролл; Каппа-??", number: 6},
+                    { word: "チリトリ", row: 0, col: 3, dir: "down", clue: "Совок", number: 7},
+                ]
+            },
+            {
+                name: "Кроссворд 2",
+                width: 4,
+                height: 4,
+                price: 60,
+                words: [
+                    { word: "ホウホウ", row: 0, col: 0, dir: "across", clue: "Способ, метод", number: 1},
+                    { word: "ホホエム", row: 0, col: 0, dir: "down", clue: "Улыбаться", number: 1.1},
+                    { word: "ホドウ", row: 1, col: 0, dir: "across", clue: "Тротуар", number: 2},
+                    { word: "ウドン", row: 0, col: 1, dir: "down", clue: "Вид лапши из пшеничной муки, характерный для японской кухни", number: 3},
+                    { word: "エンソク", row: 2, col: 0, dir: "across", clue: "Экскурсия", number: 4},
+                    { word: "ウマ", row: 3, col: 2, dir: "across", clue: "Лошадь", number: 5},
+                    { word: "クマ", row: 2, col: 3, dir: "down", clue: "Медведь", number: 6},
+                    { word: "ホウソウ", row: 0, col: 2, dir: "down", clue: "Трансляция, вещание", number: 7},
+                ]
+            },
+            {
+                name: "Кроссворд 3",
+                width: 4,
+                height: 4,
+                price: 60,
+                words: [
+                    { word: "アシ", row: 0, col: 0, dir: "across", clue: "Нога", number: 1},
+                    { word: "アサヒ", row: 0, col: 0, dir: "down", clue: "Утреннее солнце", number: 1.1},
+                    { word: "シイタケ", row: 0, col: 1, dir: "down", clue: "Гриб шиитаке", number: 2},
+                    { word: "ヒタイ", row: 2, col: 0, dir: "across", clue: "Лоб", number: 3},
+                    { word: "サイコロ", row: 1, col: 0, dir: "across", clue: "Игральная кость", number: 4},
+                    { word: "コイシ", row: 1, col: 2, dir: "down", clue: "Маленький камень", number: 5},
+                    { word: "シロ", row: 0, col: 3, dir: "down", clue: "Белый цвет", number: 6},
+                    { word: "ケシキ", row: 3, col: 1, dir: "across", clue: "Пейзаж, вид", number: 7},
+                ]
+            }
+        ]
+    };
