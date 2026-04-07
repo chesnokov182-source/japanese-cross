ac56c04534abba92932e7e22e83f3fbc18798de5
chesnokov182-source <chesnokov182@gmail.com>
2026-04-07 17:05:29 +0500

    Update n3.js

--- n3.js
+++ n3.js
@@ -16,6 +16,7 @@ window.crosswordsData.n3 = {
                 { word: "ガクシュウ", row: 1, col: 5, dir: "down", clue: "Обучение" },
                 { word: "ケンキュウ", row: 3, col: 7, dir: "down", clue: "Исследование" }
             ]
-        }
+        },
+        { "name": "Кроссворд 2", "width": 5, "height": 5, "price": 60, "words": [ { "word": "レストラン", "row": 0, "col": 0, "dir": "across", "clue": "いしそうな料理がたくさん！ 君は何を食べる？", "number": 1 }, { "word": "レイゾウコ", "row": 0, "col": 0, "dir": "down", "clue": "なかはひんやり。食べ物を保管するよ", "number": 1 }, { "word": "ライオン", "row": 0, "col": 3, "dir": "down", "clue": "動物の王様!? たてがみがかっこいいよね", "number": 3 }, { "word": "クギ", "row": 1, "col": 1, "dir": "across", "clue": "出る⑦⑨は打たれる", "number": 4 }, { "word": "ゾウ", "row": 2, "col": 0, "dir": "across", "clue": "体が大きく、はなが長いよ。バオーン！", "number": 5 }, { "word": "ウンガ", "row": 2, "col": 1, "dir": "down", "clue": "舟の移動などのためにつくられた、水の通路", "number": 6 }, { "word": "オヤ", "row": 2, "col": 3, "dir": "across", "clue": "お父さん、お母さん", "number": 7 }, { "word": "ガード", "row": 3, "col": 0, "dir": "across", "clue": "野球で、守る側の選手", "number": 8 }, { "word": "ウンテンシ", "row": 3, "col": 0, "dir": "across", "clue": "列車を運転するお仕事の人", "number": 9 }, { "word": "テラ", "row": 3, "col": 2, "dir": "down", "clue": "おぼうさんのいる建物", "number": 10 }, { "word": "コガラ", "row": 4, "col": 0, "dir": "across", "clue": "「大がら」の反対の意味の言葉は？", "number": 11 } ] }
     ]
 };
