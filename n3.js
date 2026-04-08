3fc2fb89e79a23f406ec4849b391bc670699d9f2
chesnokov182-source <chesnokov182@gmail.com>
2026-04-08 11:39:24 +0500

    Update n3.js

--- n3.js
+++ n3.js
@@ -63,6 +63,21 @@ window.crosswordsData.n3 = {
                 { word: "ナン", row: 3, col: 4, dir: "down", clue: "カレーといっしょに食べたいパン。平べったいよ", number: 10 }
       ]
     },
-        
+      {
+  name: "Кроссворд 4",
+  width: 4,
+  height: 4,
+  price: 60,
+  words: [
+    { word: "リキ", row: 0, col: 0, dir: "across", clue: "どすこい、どすこい", number: 1 },
+    { word: "ヨニン", row: 1, col: 0, dir: "across", clue: "公園に10人いましたが、6人帰りました。残りは何人？", number: 4 },
+    { word: "ウモウ", row: 2, col: 0, dir: "across", clue: "鳥のはね。ふわふわしたものが、ふとんに使われることもあるね", number: 5 },
+    { word: "ネクセ", row: 3, col: 0, dir: "across", clue: "朝起きたら、かみの毛がピヨン", number: 6 },
+    { word: "リュウコウ", row: 0, col: 0, dir: "down", clue: "今、話題や人気になっていること", number: 1 },
+    { word: "オトコノコ", row: 0, col: 1, dir: "down", clue: "男の子", number: 2 },
+    { word: "ランドセル", row: 0, col: 2, dir: "down", clue: "背中にしまって、学校へゴー！", number: 3 },
+    { word: "ウテン", row: 2, col: 3, dir: "down", clue: "雨のふる天気。「☂中止」", number: 7 }
+  ]
+}  
     ]
 };
