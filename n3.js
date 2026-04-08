0138b4047d632e5b52622fcd3f912ee95c99b450
chesnokov182-source <chesnokov182@gmail.com>
2026-04-08 11:22:36 +0500

    Update n3.js

--- n3.js
+++ n3.js
@@ -34,7 +34,7 @@ window.crosswordsData.n3 = {
                 { word: "オウチ", row: 2, col: 0, dir: "down", clue: "おうち", number: 3 },
                 { word: "モーニング", row: 3, col: 0, dir: "down", clue: "「朝」の英語", number: 4 },
                 { word: "ギンカ", row: 4, col: 0, dir: "down", clue: "銀でつくられたお金", number: 8 },
-                { word: "ジャガイモ", row: 0, col: 0, dir: "across", clue: "「ポテト」って、どんな野菜？", number: 1 },
+                { word: "ジヤガイモ", row: 0, col: 0, dir: "across", clue: "「ポテト」って、どんな野菜？", number: 1.1 },
                 { word: "ユリ", row: 0, col: 3, dir: "across", clue: "漢字で書くと「百合」。きれいなお花だよ", number: 5 },
                 { word: "エー", row: 0, col: 4, dir: "across", clue: "アルファベットの最初の文字", number: 6 },
                 { word: "ウナギ", row: 1, col: 1, dir: "across", clue: "ぬるぬるしていて、つかみにくいぞ", number: 7 },
