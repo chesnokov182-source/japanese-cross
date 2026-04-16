03c8c530f597f337d31f44ef5652ad3142279da3
chesnokov182-source <chesnokov182@gmail.com>
2026-04-16 13:53:29 +0500

    Update n1.js

--- n1.js
+++ n1.js
@@ -9,12 +9,6 @@ window.crosswordsData.n1 = {
             height: 12,
             price: 0,
             words: [
-                { word: "ジッソウ", row: 0, col: 2, dir: "across", clue: "Внедрение, реализация" },
-                { word: "ソウゴウテキ", row: 2, col: 1, dir: "across", clue: "Комплексный, всесторонний" },
-                { word: "ケイショウ", row: 4, col: 4, dir: "across", clue: "Наследование" },
-                { word: "ホショウ", row: 6, col: 0, dir: "across", clue: "Гарантия" },
-                { word: "チイキ", row: 1, col: 8, dir: "down", clue: "Регион, район" },
-                { word: "サクセイ", row: 3, col: 5, dir: "down", clue: "Составление (документа)" }
             ]
         }
     ]
