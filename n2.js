c88b97195c111e5f7ed4a8a84ff7a26d3e671178
chesnokov182-source <chesnokov182@gmail.com>
2026-04-16 13:53:19 +0500

    Update n2.js

--- n2.js
+++ n2.js
@@ -9,12 +9,6 @@ window.crosswordsData.n2 = {
             height: 10,
             price: 0,
             words: [
-                { word: "チョウサ", row: 0, col: 3, dir: "across", clue: "Расследование, исследование" },
-                { word: "ハッテン", row: 2, col: 1, dir: "across", clue: "Развитие" },
-                { word: "カンリョウ", row: 4, col: 2, dir: "across", clue: "Чиновник, бюрократ" },
-                { word: "コクサイ", row: 6, col: 0, dir: "across", clue: "Международный" },
-                { word: "ジョウホウ", row: 1, col: 5, dir: "down", clue: "Информация" },
-                { word: "イノベーション", row: 3, col: 7, dir: "down", clue: "Инновация" }
             ]
         }
     ]
